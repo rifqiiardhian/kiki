54dc37342dc00d178b9bbaac3adeac6c567c1a18
rifqiiardhian <rifqiardhian@gmail.com>
2019-10-04 15:14:49 +0700

    kiki

---
+++ private/resources/views/welcome.blade.php
@@ -0,0 +1,313 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Rifqi Ardhian Page</title>
+        <link rel="icon" type="image/png" href="{{url('assets/img/rifqiiardhian.ico')}}"/>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,600,700,800,900&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" href="{{url('assets/css/open-iconic-bootstrap.min.css')}}" />
+        <link rel="stylesheet" href="{{url('assets/css/animate.css')}}">
+    
+        <link rel="stylesheet" href="{{url('assets/css/owl.carousel.min.css')}}">
+        <link rel="stylesheet" href="{{url('assets/css/owl.theme.default.min.css')}}">
+        <link rel="stylesheet" href="{{url('assets/css/magnific-popup.css')}}">
+
+        <link rel="stylesheet" href="{{url('assets/css/aos.css')}}">
+
+        <link rel="stylesheet" href="{{url('assets/css/ionicons.min.css')}}">
+
+        <link rel="stylesheet" href="{{url('assets/css/bootstrap-datepicker.css')}}">
+        <link rel="stylesheet" href="{{url('assets/css/jquery.timepicker.css')}}">
+
+    
+        <link rel="stylesheet" href="{{url('assets/css/flaticon.css')}}">
+        <link rel="stylesheet" href="{{url('assets/css/icomoon.css')}}">
+        <link rel="stylesheet" href="{{url('assets/css/style.css')}}">
+    </head>
+    <body>
+    
+    <div class="hero-wrap" style="background-image: url('assets/img/coba2.jpg');" data-stellar-background-ratio="0.5">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-start" data-scrollax-parent="true">
+          <div class="col-lg-6 col-md-6 ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+            <h1 class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"> Biodata Page <br><span>Rifqi Ardhian</span></h1>
+          </div>
+          <div class="col-lg-2 col"></div>
+          <div class="col-lg-4 col-md-6 mt-0 mt-md-5">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <section class="ftco-section services-section bg-primary">
+      <div class="container">
+        <div class="row d-flex">
+          <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services d-block">
+              <div class="icon"><img src = "{{url('assets/img/computer.png')}}" style = "width: 70px; height: 70px"></div>
+              <br>
+              <div class="media-body">
+                <h3 class="heading mb-3">Web Design</h3>
+                <p>Design is so simple. That's why it's so complicated.</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services d-block">
+            <div class="icon"><img src = "{{url('assets/img/data.png')}}" style = "width: 70px; height: 70px"></div>
+            <br>
+              <div class="media-body">
+                <h3 class="heading mb-3">Web Development</h3>
+                <p>We love what we do and we do what our clients love & work with great clients all over the world to create thoughtful and purposeful websites.</p>
+              </div>
+            </div>    
+          </div>
+          <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services d-block">
+            <div class="icon"><img src = "{{url('assets/img/creativity.png')}}" style = "width: 70px; height: 70px"></div>
+            <br>
+              <div class="media-body">
+                <h3 class="heading mb-3">Sketch and Typography</h3>
+                <p>Drawing is not what one sees but what one can make others see. When typography is on point, words become images.</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services d-block">
+            <div class="icon"><img src = "{{url('assets/img/happiness.png')}}" style = "width: 70px; height: 70px"></div>
+            <br>
+              <div class="media-body">
+                <h3 class="heading mb-3">Smile For Everyone</h3>
+                <p>One small good is smile for other. Lets start the Shodaqoh with a smile</p>
+              </div>
+            </div>      
+          </div>
+        </div>
+      </div>
+    </section>
+
+    
+   	
+		<section class="ftco-section ftco-no-pt ftco-no-pb">
+			<div class="container">
+				<div class="row no-gutters">
+					<div class="col-md-5 p-md-5 img img-2 d-flex justify-content-center align-items-center">
+                        <img src = "{{url('assets/img/about.jpg')}}" style = "width: 300px; height: 400px" class = "img-fluid rounded">
+					</div>
+					<div class="col-md-7 wrap-about py-md-5 ftco-animate">
+	          <div class="heading-section mb-5 pt-5 pl-md-5">
+	          	<div class="pr-md-5 mr-md-5">
+		            <h2 class="mb-4">About Me</h2>
+	            </div>
+	            <p>Hi everyone !, i'm Rifqi Ardhian and welcome to my webpage "Rifqi Ardhian Biodata Page"</p>
+              				<p>We love what we do and we do what our clients love & work with great clients all over 
+                				the world to create thoughtful and purposeful websites.</p>
+                			<p>i hope you impressed and interest on my website. Special thanks to all of you who has 
+                    			seen my page, do what you love and so many reasons to be happy. 
+								From Rifqi Ardhian with <i class="icon-heart-o" aria-hidden="true"></i></p>
+	          </div>
+					</div>
+				</div>
+			</div>
+        </section>
+
+        <br/>
+
+        <section class="ftco-section ftco-no-pt ftco-no-pb">
+        <div class="parallax-img d-flex align-items-center">
+        <div class="container">
+        <div class = "row d-flex justify-content-center">
+            <div class="col-md-10 col-lg-7 text-center heading-section ftco-animate">
+              <h2>Details</h2>
+            </div>
+        </div>
+        <br><br>
+        <div class = "row d-flex justify-content-">
+            <div class = "col-md-6">
+            <table>
+                <tr>
+                    <td>Born</td>
+                    <td> : </td>
+                    <td>October 13, 2001</td>
+                </tr>
+                <tr>
+                    <td>Address</td>
+                    <td> : </td>
+                    <td>Jalan Bendungan Wlingi 30 Malang 65145</td>
+                </tr>
+                <tr>
+                    <td>Sex</td>
+                    <td> : </td>
+                    <td>Male</td>
+                </tr>
+            </table>
+            </div>
+            <div class = "col-md-6">
+                <strong>Personal Description</strong>
+                <p>Work hard and have high commitment.  Responsible. Perfectionist in every work until forget the time</p>
+            </div>
+        </div>
+        </div>  
+    </section>
+
+        <br /><br />
+
+    <section class="ftco-section-parallax ftco-section ftco-no-pt">
+            
+      <div class="parallax-img d-flex align-items-center">
+        <div class="container">
+        <div class = "row d-flex justify-content-center">
+            <div class="col-md-10 col-lg-7 text-center heading-section ftco-animate">
+              <h2>Education</h2>
+            </div>
+        </div>
+        <br><br>
+          <div class="row d-flex justify-content-center">
+              <div class = "col-md-6">
+                <div class="speaker ftco-animate speaker-1 d-flex align-items-center mb-5">
+	        		<img src = "{{url('assets/img/min1.jpg')}}" style = "width: 100px; height: 100px">
+	        		<div class="text pl-4">
+	        			<h3>Madrasah Ibtidaiyah Negeri I Malang</h3>
+                        <p>-</p>
+                        <span class="position">2007 - 2013</span>
+	        		</div>
+                </div>
+              </div>
+              <div class = "col-md-6">
+                <div class="speaker ftco-animate speaker-1 d-flex align-items-center mb-5">
+	        		<img src = "{{url('assets/img/mtsn1.jpg')}}" style = "width: 100px; height: 100px">
+	        		<div class="text pl-4">
+	        			<h3>Madrasah Tsanawiyah Negeri I Malang</h3>
+                        <p>-</p>
+                        <span class="position">2013 - 2015</span>
+	        		</div>
+                </div>
+              </div>
+          </div>
+          <div class="row d-flex justify-content-center">
+              <div class = "col-md-6">
+                <div class="speaker ftco-animate speaker-1 d-flex align-items-center mb-5">
+	        		<img src = "{{url('assets/img/sman8.jpg')}}" style = "width: 100px; height: 100px">
+	        		<div class="text pl-4">
+	        			<h3>Sekolah Menengah Atas Negeri 8 Malang</h3>
+                        <p>-</p>
+                        <span class="position">2015 - 2018</span>
+	        		</div>
+                </div>
+              </div>
+              <div class = "col-md-6">
+                <div class="speaker ftco-animate speaker-1 d-flex align-items-center mb-5">
+	        		<img src = "{{url('assets/img/vokasi.png')}}" style = "width: 100px; height: 100px">
+	        		<div class="text pl-4">
+	        			<h3>Universitas Brawijaya</h3>
+                        <p>DIII Sistem Informasi</p>
+                        <span class="position">2018 - Present</span>
+	        		</div>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="ftco-section-parallax ftco-section ftco-no-pt">
+            
+            <div class="parallax-img d-flex align-items-center">
+              <div class="container">
+              <div class = "row d-flex justify-content-center">
+                  <div class="col-md-10 col-lg-7 text-center heading-section ftco-animate">
+                    <h2>Experiences</h2>
+                  </div>
+                  <br /><br />
+                  <div class="col-md-12">
+                    <ul>
+                        <li>Komisi Pengawasan dan Evaluasi MPK SMAN 8 Malang 2015 / 2016</li>
+                        <li>Ketua Umum MPK SMAN 8 Malang 2016 / 2017</li>
+                        <li>PSDM HIMMASI UB 2018 / 2019</ol>
+                        <li>Wakil Ketua HIMMASI UB 2019 / 2020</li>
+                    </ul>
+                  </div>
+              </div>
+            </div>
+          </section>
+
+		<section class="ftco-section-parallax ftco-section ftco-no-pt">
+      <div class="parallax-img d-flex align-items-center">
+        <div class="container">
+          <div class="row d-flex justify-content-center">
+            <div class="col-md-10 col-lg-7 text-center heading-section ftco-animate">
+              <h2>Thank you for visiting my page</h2>
+              <p>We hope you interest about my biography</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer class="ftco-footer ftco-bg-dark ftco-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">FROM RIFQI ARDHIAN WITH <i class="icon-heart color-danger" aria-hidden="true"></i></h2>
+              <p>Special thanks to all of you who has seen my page</p>
+              <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+                <li class="ftco-animate"><a href="https://twitter.com/rifqi_ardhian"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="https://web.facebook.com/people/Rifqi-Ardhian/100009201050672?_rdc=1&_rdr"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="https://www.instagram.com/rifqiiardhian/"><span class="icon-instagram"></span></a></li>
+                <li class="ftco-animate"><a href="tel:+6281334457150"><span class="icon-whatsapp"></span></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+          </div>
+          <div class="col-md">
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+            	<h2 class="ftco-heading-2">Contact Me</h2>
+            	<div class="block-23 mb-3">
+	              <ul>
+	                <li><span class="icon icon-map-marker"></span><span class="text">Jalan Bendungan Wlingi 30 Malang 65145</span></li>
+	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+6281 334 457 150 </span></a></li>
+	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">rifqiardhian@gmail.com</span></a></li>
+	              </ul>
+	            </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <p>Rifqi Ardhian Biodata Page</p>
+            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved</a>
+  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+  <script src="{{url('assets/js/jquery.min.js')}}"></script>
+  <script src="{{url('assets/js/jquery-migrate-3.0.1.min.js')}}"></script>
+  <script src="{{url('assets/js/popper.min.js')}}"></script>
+  <script src="{{url('assets/js/bootstrap.min.js')}}"></script>
+  <script src="{{url('assets/js/jquery.easing.1.3.js')}}"></script>
+  <script src="{{url('assets/js/jquery.waypoints.min.js')}}"></script>
+  <script src="{{url('assets/js/jquery.stellar.min.js')}}"></script>
+  <script src="{{url('assets/js/owl.carousel.min.js')}}"></script>
+  <script src="{{url('assets/js/jquery.magnific-popup.min.js')}}"></script>
+  <script src="{{url('assets/js/aos.js')}}"></script>
+  <script src="{{url('assets/js/jquery.animateNumber.min.js')}}"></script>
+  <script src="{{url('assets/js/scrollax.min.js')}}"></script>
+  <script src="{{url('assets/js/google-map.js')}}"></script>
+  <script src="{{url('assets/js/main.js')}}"></script>
+    
+    </body>
+</html>
